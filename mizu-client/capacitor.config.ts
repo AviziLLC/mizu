3759f372e981ba7cfe8b927b8a764e3e1a641621
Avizi Developer <admin@avizi.co>
2026-02-05 14:46:54 -0800

    WIP

---
+++ mizu-client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.avizi.mizu',
+  appName: 'Mizu',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ mizu-client/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader, IonCardSubtitle, IonCardTitle,
+    IonContent,
+    IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList,
+    IonListHeader,
+    IonPage, IonRange, IonSelect, IonSelectOption, IonText,
+    IonTitle, IonToggle,
+    IonToolbar
+} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+import {moon, personCircle, sunny} from "ionicons/icons";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
