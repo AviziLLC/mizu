b8a7bbbed86ba1bdd77c3444f6b7902f1fb1efb5
Avizi Developer <admin@avizi.co>
2026-02-05 14:49:57 -0800

    Remove unused imports

--- mizu-client/src/pages/Home.tsx
+++ mizu-client/src/pages/Home.tsx
@@ -1,19 +1,8 @@
 import {
-    IonButton,
-    IonButtons,
-    IonCard,
-    IonCardContent,
-    IonCardHeader, IonCardSubtitle, IonCardTitle,
     IonContent,
-    IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList,
-    IonListHeader,
-    IonPage, IonRange, IonSelect, IonSelectOption, IonText,
-    IonTitle, IonToggle,
-    IonToolbar
+    IonPage
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import {moon, personCircle, sunny} from "ionicons/icons";
 
 const Home: React.FC = () => {
   return (
