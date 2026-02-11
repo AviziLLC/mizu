2fb9313d6afd196b0a41564c4b107339eff5bf95
Avizi Developer <support@avizi.co>
2026-02-11 10:04:33 -0800

    Redesign home page

--- mizu-client/src/pages/Home.tsx
+++ mizu-client/src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import {
     IonButton,
-    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
     IonContent,
-    IonPage
+    IonPage,
+    IonTitle
 } from '@ionic/react';
 import './Home.css';
 import {
-    IoBrowsersOutline,
-    IoCreate,
     IoCreateOutline,
     IoSearchOutline,
-    IoSettings,
     IoSettingsOutline
 } from "react-icons/io5";
 
@@ -19,44 +21,48 @@ const Home: React.FC = () => {
     return (
         <IonPage>
             <IonContent>
-                <div className={'ion-display-flex ion-flex-column ion-justify-content-center ion-align-items-center'}>
-                    <IonCard>
-                        <IonCardContent>
-                            <div className={'ion-display-flex ion-flex-row'}>
-                                <IonButton className={'ion-margin-end ion-display-flex ion-align-items-center'} color={'primary'}>
-                                    <IoCreateOutline style={{marginRight: 5, marginBottom: 4, width: 24, height: 24,}}/>
-                                    Create Deck
-                                </IonButton>
-                                <IonButton className={'ion-margin-end'} color={'secondary'}>
-                                    <IoSearchOutline style={{marginRight: 5, width: 24, height: 24}}/>
-                                    Card Browser
-                                </IonButton>
-                                <IonButton color={'tertiary'}>
-                                    <IoSettingsOutline style={{marginRight: 5, width: 24, height: 24}}/>
-                                    <span>Settings</span>
-                                </IonButton>
-                            </div>
-                        </IonCardContent>
-                    </IonCard>
+                <div className={'ion-padding home-container'}>
+                    <div className={'ion-padding ion-display-flex ion-justify-content-center'} style={{gap: '1rem'}}>
+                        <IonButton>
+                            <IoCreateOutline slot="start"/>
+                            Create Deck
+                        </IonButton>
+                        <IonButton color={'light'}>
+                            <IoSearchOutline slot="start"/>
+                            Card Browser
+                        </IonButton>
+                        <IonButton color={'light'}>
+                            <IoSettingsOutline slot="start"/>
+                            Settings
+                        </IonButton>
+                    </div>
 
-                    <IonCard className={'ion-padding'} style={{minWidth: '50%'}}>
-                        <IonCardHeader>
-                            <IonCardTitle>
-                                <div className={'ion-display-flex ion-flex-row ion-justify-content-center'}>
-                                    <span>Decks</span>
-                                </div>
-                            </IonCardTitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            {decks.map((deck) => (
-                                <div className={'ion-margin-bottom ion-padding ion-display-flex ion-flex-column ion-align-items-start'}>
-                                    <h1>{deck}</h1>
-                                    <IonButton color={'primary'}>Study</IonButton>
-                                    <IonButton color={'secondary'}>Add Cards</IonButton>
-                                </div>
-                            ))}
-                        </IonCardContent>
-                    </IonCard>
+                    <div className={'ion-text-center ion-padding-top'}>
+                        <IonTitle>Decks</IonTitle>
+                    </div>
+
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '1rem',
+                        paddingTop: '1rem'
+                    }}>
+                        {decks.map((deck) => (
+                            <IonCard key={deck} style={{minWidth: '50%'}}>
+                                <IonCardHeader>
+                                    <IonCardTitle>{deck}</IonCardTitle>
+                                    <IonCardSubtitle>X cards due, Y new</IonCardSubtitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    <div className={'ion-display-flex ion-justify-content-end'}>
+                                        <IonButton fill="clear">Add Cards</IonButton>
+                                        <IonButton>Study</IonButton>
+                                    </div>
+                                </IonCardContent>
+                            </IonCard>
+                        ))}
+                    </div>
                 </div>
             </IonContent>
         </IonPage>
